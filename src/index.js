4cea380f946c81748fcdcfa3f46e9b8e964be5d1
Kamal Ahmed <kamalacca@gmail.com>
2022-09-04 10:05:39 +0545

    add rule for handling SCSS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import sayHello from "./components/module";
 import showImage from "./components/imagemodule";
 import './components/style.css';
+import './components/sass-style-example.scss';
 
 showImage();
 sayHello('Kamal');
--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,13 @@ module.exports = {
                 use: [
                     'style-loader', 'css-loader'
                 ]
-            }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader', 'css-loader', 'sass-loader'
+                ]
+            },
         ]
     }
 
